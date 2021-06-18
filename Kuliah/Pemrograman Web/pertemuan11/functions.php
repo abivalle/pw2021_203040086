afcb38f7776938a202e7b68984c4de866d02741e
abivalle <75163195+abivalle@users.noreply.github.com>
2021-06-18 22:53:07 +0700

    Add files via upload

---
+++ Kuliah/Pemrograman Web/pertemuan11/functions.php
@@ -0,0 +1,91 @@
+<?php 
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'phpdasar');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+
+  $result = mysqli_query($conn, $query);
+
+  //jika hasilnya hanya 1 data
+  if( mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) 
+  {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+function tambah($data) 
+{
+  $conn = koneksi();
+
+  $nama = htmlspecialchars($data['nama']);
+  $npm  = htmlspecialchars($data['npm']);
+  $prodi = htmlspecialchars($data['prodi' ]);
+  $email = htmlspecialchars($data['email']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "INSERT INTO
+            mahasiswa
+            VALUES
+            (null, '$nama', '$npm', '$prodi', '$email', '$gambar');
+            ";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  echo mysqli_error($conn);
+  return mysqli_affected_rows($conn);
+}
+
+function hapus($id)
+{
+  $conn = koneksi();
+  mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id") or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+  $conn = koneksi();
+
+  $id = $data['id'];
+  $nama = htmlspecialchars($data['nama']);
+  $npm  = htmlspecialchars($data['npm']);
+  $prodi = htmlspecialchars($data['prodi']);
+  $email = htmlspecialchars($data['email']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "UPDATE mahasiswa SET
+            nama = '$nama',
+            npm = '$npm,
+            prodi = '$prodi',
+            email = '$email',
+            gambar = '$gambar'
+            WHERE id = $id";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  echo mysqli_error($conn);
+  return mysqli_affected_rows($conn);
+}
+
+function cari($keyword) {
+  $conn = koneksi();
+
+  $query = "SELECT * FROM mahasiswa
+              WHERE 
+              nama LIKE '%$keyword%' OR 
+              npm LIKE '%$keyword%'";
+  $result = mysqli_query($conn, $query);
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+?>
